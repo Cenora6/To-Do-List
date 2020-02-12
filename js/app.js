680ab2af732de28ce5e9fd9056c36380f5a091e8
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-02-12 14:22:49 +0100

    Added input focus styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM');
 
+    const form = document.getElementsByClassName('toDoForm')[0];
+    const input = document.getElementById('taskInput');
+
+    input.addEventListener('focus', function () {
+        form.classList.add('focusInput');
+    });
+
+    input.addEventListener('focusout', function () {
+        form.classList.remove('focusInput')
+    })
+
+});
